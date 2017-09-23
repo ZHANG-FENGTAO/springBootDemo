28a81850864c91d7e65c3ff26b6ff43be61a710a
zhangfengtao <zhangfengtao@ejiaoji.com>
2017-09-23 10:10:49 +0800

    修改配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
-@ComponentScan(basePackages = {"com.example.demo.","com.example.ueditor"})
+@ComponentScan(basePackages = {"com.example.demo","com.example.ueditor"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
